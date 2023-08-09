c8259e71503b511724eae2b27b5c334a62189374
Amneesh Singh <natto@weirdnatto.in>
2023-08-09 09:52:24 +0530

    cv: add hrule after section headings
    Signed-off-by: Amneesh Singh <natto@weirdnatto.in>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My personal website";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/release-22.05;
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
     utils.url = github:numtide/flake-utils;
   };
 
