26e5a19ae67ec8fb6f3d610b1c18795ee8e81ff8
Amneesh Singh <natto@weirdnatto.in>
2026-03-01 02:37:47 +0530

    subdomains: update
    Signed-off-by: Amneesh Singh <natto@weirdnatto.in>

--- src/Main.hs
+++ src/Main.hs
@@ -248,7 +248,7 @@ defaultCtx =
     domainCtx :: Context String
     domainCtx = constField "domain" domain
     subdomains :: [Item String]
-    subdomains = map mkItem ["git", "radio", "f"]
+    subdomains = map mkItem ["git", "radio", "f", "map"]
       where
         mkItem :: a -> Item a
         mkItem a = Item{itemIdentifier = "subdomain", itemBody = a}
